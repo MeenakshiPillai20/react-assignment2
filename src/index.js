c18a32a6bdff808cee19733385406bc6d557a80d
MeenakshiPillai20 <pillai.meenakshi20@gmail.com>
2022-10-28 19:33:56 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Form from './Form';
+import App from './App';
+
 import './style.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Form/>
+   <App/>
   </React.StrictMode>
 );
